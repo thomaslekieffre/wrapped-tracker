db76ff5404c1b37f32a3a16572841ce63d0b9bbb
thomaslekieffre <thomaslekieffre59.dev@gmail.com>
2025-02-13 15:33:47 +0100

    fix: eslint

--- src/hooks/use-toast.ts
+++ src/hooks/use-toast.ts
@@ -22,13 +22,6 @@ type ActionTypes = {
   REMOVE_TOAST: 'REMOVE_TOAST';
 };
 
-const actionTypes: ActionTypes = {
-  ADD_TOAST: 'ADD_TOAST',
-  UPDATE_TOAST: 'UPDATE_TOAST',
-  DISMISS_TOAST: 'DISMISS_TOAST',
-  REMOVE_TOAST: 'REMOVE_TOAST',
-} as const;
-
 let count = 0;
 
 function genId() {
